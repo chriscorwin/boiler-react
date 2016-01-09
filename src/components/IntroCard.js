1194f4f1e1cc217a272af24a2c2971d58d3f1043
Richard Boardman <rboardman@salesforce.com>
2016-01-08 17:40:08 -0800

    Fixed card header markup

--- src/components/IntroCard.js
+++ src/components/IntroCard.js
@@ -8,13 +8,19 @@ export class IntroCard extends React.Component {
       <div className="slds-col--padded slds-size--1-of-1 slds-large-size--8-of-12">
         <div className="slds-card slds-theme--alert-texture">
           <div className="slds-card__header slds-grid">
-            <h3 className="slds-text-heading--small">Internal Development</h3>
+            <div className="slds-media slds-media-center">
+              <div className="slds-media__figure">
+                <SLDSIcons.Icon assistiveText=""
+                    category="utility"
+                    className="slds-icon-text-default"
+                    name="like"
+                    size="small" />
+              </div>
+              <div class="slds-media__body">
+                <h2 class="slds-text-heading--small slds-truncate">Card Header (2)</h2>
+              </div>
+            </div>
           </div>  
-          <SLDSIcons.Icon assistiveText=""
-            category="utility"
-            className="slds-icon-text-default"
-            name="like"
-            size="small" />
           <p className="slds-text-body--small slds-m-horizontal--small">Internal development builds are available for use by internal customers. These reflect the state of the Salesforce product in future releases, and are prone to change in accordance with shifting priorities and designs throughout a release, and may contain components that we do not yet want to surface to external partners and customers.</p>
         </div>
       </div>
