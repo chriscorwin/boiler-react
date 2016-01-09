661d28737808e3c677fb61e089d6793c84c1d9bf
Richard Boardman <rboardman@salesforce.com>
2016-01-08 17:46:18 -0800

    Fixed card layout

--- src/components/IntroCard.js
+++ src/components/IntroCard.js
@@ -6,8 +6,8 @@ export class IntroCard extends React.Component {
   render() {
     return (
       <div className="slds-col--padded slds-size--1-of-1 slds-large-size--8-of-12">
-        <div className="slds-card slds-theme--alert-texture">
-          <div className="slds-card__header slds-grid">
+        <div className="slds-card slds-theme--alert-texture slds-m-vertical--large">
+          <div className="slds-card__header slds-grid ">
             <div className="slds-media slds-media-center">
               <div className="slds-media__figure">
                 <SLDSIcons.Icon assistiveText=""
@@ -21,7 +21,7 @@ export class IntroCard extends React.Component {
               </div>
             </div>
           </div>  
-          <p className="slds-text-body--small slds-m-horizontal--small">Internal development builds are available for use by internal customers. These reflect the state of the Salesforce product in future releases, and are prone to change in accordance with shifting priorities and designs throughout a release, and may contain components that we do not yet want to surface to external partners and customers.</p>
+          <p className="slds-text-body--small slds-m-horizontal--small slds-m-bottom--large">Internal development builds are available for use by internal customers. These reflect the state of the Salesforce product in future releases, and are prone to change in accordance with shifting priorities and designs throughout a release, and may contain components that we do not yet want to surface to external partners and customers.</p>
         </div>
       </div>
     )
