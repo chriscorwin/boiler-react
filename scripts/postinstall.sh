20e01e91a13db24a31c818ee6d8ec19f330c1276
Richard Boardman <rboardman@salesforce.com>
2016-01-21 11:41:07 -0800

    Get it building on heroku

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,1 +1,13 @@
+if [ -n "$HEROKU_APP_NAME" ]
+then
+  git clone https://$GITHUB_USER:$GITHUB_USER_ACCESS_TOKEN@$DESIGN_SYSTEM_REACT_REPO
+else
+  git clone git@github.com:salesforce-ux/design-system-react.git
+fi
+
+mv design-system-react node_modules
+cd node_modules/design-system-react
+npm install
+cd ../..
+
 cp -r node_modules/@salesforce-ux/design-system/assets public/
