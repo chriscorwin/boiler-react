34f2841f68cd0d867b1df979e1e78d04bf92fdf7
Donielle Berg <donielle.berg@gmail.com>
2016-02-29 15:14:24 -0800

    Remove things

--- src/components/pages/CartPage/Header.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export class Header extends Component {
-  render() {
-    return <div className="slds-p-around--large">
-      <h1 className="slds-text-heading--large">Fruite Shoppe</h1>
-    </div>;
-  }
-}
--- src/main.js
+++ src/main.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Router, { Route, DefaultRoute, RouteHandler, browserHistory } from 'react-router';
 
-import { CartPage } from 'components/pages/CartPage';
-import { IntroCard } from 'components/pages/IntroCard';
-import { SpreadsheetPage } from 'components/pages/SpreadsheetPage';
 import { WelcomePage } from 'components/pages/WelcomePage';
 
 render(
-  <Router history={browserHistory}>
+  <Router>
     <Route path="/" component={WelcomePage} />
-    <Route path="/intro" name="intro" component={IntroCard} />
-    <Route path="/cart" name="cart" component={CartPage} />
-    <Route path="/spreadsheet" name="spreadsheet" component={SpreadsheetPage} />
   </Router>,
-  document.getElementById("examples")
+  document.getElementById("app")
 );
